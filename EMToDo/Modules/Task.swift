6c7aa6b99b053fe147988ff3aa997f8caf76bfcc
MaximMacarone <maximmacarone@gmail.com>
2025-03-25 13:40:07 +0300

    task list prototype

---
+++ EMToDo/Modules/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  EMToDo
+//
+//  Created by Maxim Makarenkov on 25.03.2025.
+//
+
+import Foundation
+
+struct Task {
+    let title: String
+    let description: String
+    let createdAt: Date = Date()
+}
---
+++ EMToDo/Modules/TaskList/TaskListViewDescription.swift
@@ -0,0 +1,10 @@
+//
+//  TaskListViewDescription.swift
+//  EMToDo
+//
+//  Created by Maxim Makarenkov on 25.03.2025.
+//
+
+protocol TaskListViewDescription {
+    
+}
